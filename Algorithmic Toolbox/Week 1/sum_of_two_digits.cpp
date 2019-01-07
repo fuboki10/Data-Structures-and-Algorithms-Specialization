668a62a1d5cf66c1b83b3b4f5d99a1c5100d4fca
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-01-07 17:24:22 +0200

    sum_of_two_digits

---
+++ Algorithmic Toolbox/Week 1/sum_of_two_digits.cpp
@@ -0,0 +1,23 @@
+// Fub0ki
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define  mp make_pair
+#define  pb push_back
+#define bn begin()
+#define nd end()
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define EPS 1e-7
+
+int main()
+{
+    //freopen("simple.in", "r", stdin);
+    int a, b;
+	cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
