e2689b1959bf6043ef0468b6aa042fcfa3362066
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-01-27 14:12:49 +0200

    week2 assig 5

---
+++ Algorithmic Toolbox/Week 2/Fibonacci_Number_Again.cpp
@@ -0,0 +1,50 @@
+// Fub0ki
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define  mp make_pair
+#define  pb push_back
+#define bn begin()
+#define nd end()
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define EPS 1e-7
+
+long long get_pisano_period(long long m) {
+    long long a = 0, b = 1, c = a + b;
+    for (int i = 0; i < m * m; i++) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1) return i + 1;
+    }
+}
+
+long long get_fibonacci_huge(long long n, long long m) {
+    long long remainder = n % get_pisano_period(m);
+
+    long long first = 0;
+    long long second = 1;
+
+    long long res = remainder;
+
+    for (int i = 1; i < remainder; i++) {
+        res = (first + second) % m;
+        first = second;
+        second = res;
+    }
+
+    return res % m;
+}
+
+int main()
+{
+    //freopen("simple.in", "r", stdin);
+    long long n, m;
+    cin >> n >> m;
+    cout << get_fibonacci_huge(n, m) << endl;
+    return 0;
+}
