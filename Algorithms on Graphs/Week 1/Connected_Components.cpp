218324d32e7b9a3430b29272e78c71bded26dc37
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-06-08 16:20:36 +0200

    Algo graph week 1

---
+++ Algorithms on Graphs/Week 1/Connected_Components.cpp
@@ -0,0 +1,91 @@
+// Fub0ki
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long             ll;
+typedef vector<int>           vi;
+typedef vector<ll>            vll;
+typedef pair<int, int>        ii;
+typedef vector<ii>            vii;
+typedef set<int>              si;
+typedef map<string, int>      msi;
+typedef map<string, string>   mss;
+#define clr(x,v) memset(x,v,sizeof(x))
+#define mp make_pair
+#define pb push_back
+#define mt make_tuple
+#define eb emplace_back
+#define bn begin()
+#define nd end()
+#define X first
+#define Y second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define NumofDigits(n) ((int)log10(n)+1)
+#define sc1(x)  scanf("%d",&x)
+#define sc2(x,y)  scanf("%d%d",&x,&y)
+#define sc3(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define sz(v) (int)(v.size())
+#define rep(i,v) for (int i = 0; i < sz(v); i++)
+#define lp(i, a, b) for (int i = int(a); i < int(b); i++)
+#define lpi(i, b, a) for (int i = int(b); i >= a; i--)
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define MAX 1000005
+#define INF 1e18
+#define print_prec(ans,num) cout << fixed << setprecision(num) << (ans) << endl
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL)
+const int dx[] = { 1,-1,0,0,0,0 };
+const int dy[] = { 0,0,1,-1,0,0 };
+const int dz[] = { 0,0,0,0,1,-1 };
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistic*/
+/*max_element(all(m),
+    [](const ii& p1, const ii& p2) {
+        return p1.second < p2.second; });*/
+int n,m;
+bool vis[MAX];
+vi nodes[MAX];
+void dfs(int cur)
+{
+    vis[cur] = 1;
+    for (auto u : nodes[cur])
+    {
+        if (!vis[u])
+        {
+            dfs(u);
+        }
+    }
+}
+
+int main()
+{
+    //freopen("read.txt", "r", stdin);
+    //freopen("write.txt", "w", stdout);
+    IO;
+    cin >> n >> m;
+    int u,v;
+    while (m--)
+    {
+        cin >> u >> v;
+        nodes[u].pb(v);
+        nodes[v].pb(u);
+    }
+    int cnt = 0;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (!vis[i])
+        {
+            cnt++;
+            dfs(i);
+        }
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
